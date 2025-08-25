b21f6a63a3a4947caf5eb4fe6ecc8848ce3b0703
Dylan H. Morris <dylan@dylanhmorris.com>
2025-08-25 17:13:23 -0400

    DRYify test

--- test/test_gather.py
+++ test/test_gather.py
@@ -1,21 +1,34 @@
 import pytest
 import arviz as az
+import polars as pl
 from polarbayes.gather import gather_draws
 
 
-@pytest.fixture
-def rugby_data():
-    return az.load_arviz_data("rugby")
+rugby_field_data = az.load_arviz_data("rugby_field")
 
 
-def test_gather_mixed_indices(rugby_data):
+@pytest.mark.parametrize(
+    ["data", "test_vars"],
+    [
+        [
+            rugby_field_data,
+            {
+                "intercept": {"team": False, "field": True},
+                "defs": {"team": True, "field": True},
+                "sd_def_field": {"team": False, "field": False},
+            },
+        ]
+    ],
+)
+def test_gather_mixed_indices(data, test_vars):
     """
     Test that `gather_draws()` handles
     mixed indices gracefully, with null
     where a variable is not indexed
     """
-    result = gather_draws(rugby_data, "posterior")
-    data_vars = list(rugby_data.posterior.keys())
+    print(data)
+    result = gather_draws(data, "posterior")
+    data_vars = list(data.posterior.keys())
     # all and only variables from posterior group should
     # be present in the variable column
     assert all([v in result["variable"] for v in data_vars])
@@ -26,8 +39,21 @@ def test_gather_mixed_indices(rugby_data):
     # be ints
     assert result["chain"].dtype.is_integer()
     assert result["draw"].dtype.is_integer()
-    assert not result["chain"].is_null().any()
-    assert not result["draw"].is_null().any()
+    assert result["chain"].is_not_null().all()
+    assert result["draw"].is_not_null().any()
+
+    # variables not indexed by a given column should have null values for that index column
+    # while those that are should have non-null values
+    for var, index_col_dict in test_vars.items():
+        df_filt = result.filter(pl.col("variable") == var)
+        assert df_filt.height > 0
+        for i_col, is_used in index_col_dict.items():
+            assert i_col in result.columns
+            col = df_filt.get_column(i_col)
+            if is_used:
+                assert col.is_not_null().all()
+            else:
+                assert col.is_null().all()
 
 
 def test_gather_mixed_types():
