c8e7ade78476d087ba0099456488814413eedddf
Dylan H. Morris <dylan@dylanhmorris.com>
2025-08-26 11:29:49 -0400

    Tests for non-defaults, improve defaults

--- polarbayes/schema.py
+++ polarbayes/schema.py
@@ -4,12 +4,17 @@ Column order schemas for polarbayes output
 
 from typing import Iterable
 
+DEFAULT_CHAIN_COLUMN = "chain"
+DEFAULT_DRAW_COLUMN = "draw"
+
 
 def order_index_column_names(
-    index_columns: Iterable[str], chain_id_col: str = "chain", draw_id_col: str = "draw"
-) -> Iterable[str]:
+    index_columns: Iterable[str],
+    chain_id_col: str | None = None,
+    draw_id_col: str | None = None,
+) -> list[str]:
     """
-    Order a list of index column names by placing the reserved
+    Order an iterable of index column names by placing the reserved
     names for the chain and draw ids first and then sorting
     additional index columns alphabetically.
 
@@ -20,19 +25,24 @@ def order_index_column_names(
 
     chain_id_col
         Reserved name for the chain ID column. Will always
-        be placed first in the order, if present. Default
-        `"chain"`
+        be placed first in the order, if present. If `None`
+        (default), use `"chain"`
 
     draw_id_col
         Reserved name for the draw ID column. Will always
         be placed immediately after the `chain_id_col`
-        in the order, if present. Default `"draw"`.
+        in the order, if present. Default if `None` (default),
+        use `"draw"`.
 
     Returns
     -------
-    The column names, ordered, with the reserved names first
+    A list of column names, ordered, with the reserved names first
     and then additional column names ordered alphabetically.
     """
+    if chain_id_col is None:
+        chain_id_col = DEFAULT_CHAIN_COLUMN
+    if draw_id_col is None:
+        draw_id_col = DEFAULT_DRAW_COLUMN
     return sorted(
         index_columns,
         key=lambda x: {chain_id_col: (0, 0), draw_id_col: (1, 0)}.get(x, (2, x)),
--- test/test_schema.py
+++ test/test_schema.py
@@ -3,14 +3,41 @@ from polarbayes import schema as s
 
 
 @pytest.mark.parametrize(
-    ["input", "expected"],
+    ["input", "expected", "chain_id_col", "draw_id_col"],
     [
-        (["a", "0b", "draw", "chain"], ["chain", "draw", "0b", "a"]),
-        (["a", "0b", "b"], ["0b", "a", "b"]),
-        (["draw", "chain"], ["chain", "draw"]),
-        (["a", "0b", "chain"], ["chain", "0b", "a"]),
-        (["chain", "draw", "a"], ["chain", "draw", "a"]),
+        (["a", "0b", "draw", "chain"], ["chain", "draw", "0b", "a"], None, None),
+        (
+            ["chain", "draw", "a", "0b", "custom_draw_name", "custom_chain_name"],
+            ["custom_chain_name", "custom_draw_name", "0b", "a", "chain", "draw"],
+            "custom_chain_name",
+            "custom_draw_name",
+        ),
+        (["a", "0b", "b"], ["0b", "a", "b"], None, None),
+        (["a", "0b", "b"], ["0b", "a", "b"], "custom_chain_name", "custom_draw_name"),
+        (["draw", "chain"], ["chain", "draw"], None, None),
+        (["a", "0b", "chain"], ["chain", "0b", "a"], None, None),
+        (["chain", "draw", "a"], ["chain", "draw", "a"], None, None),
+        (["a", "0b", "chain"], ["0b", "a", "chain"], "custom_chain_name", None),
+        (["draw", "chain"], ["chain", "draw"], "custom_chain_name", "custom_draw_name"),
     ],
 )
-def test_order_index_column_names(input, expected):
-    assert s.order_index_column_names(input) == expected
+def test_order_index_column_names(input, expected, chain_id_col, draw_id_col):
+    assert (
+        s.order_index_column_names(
+            input, chain_id_col=chain_id_col, draw_id_col=draw_id_col
+        )
+        == expected
+    )
+    # check it works with different types of iterable, not just lists.
+    assert (
+        s.order_index_column_names(
+            iter(input), chain_id_col=chain_id_col, draw_id_col=draw_id_col
+        )
+        == expected
+    )
+    assert (
+        s.order_index_column_names(
+            (x for x in input), chain_id_col=chain_id_col, draw_id_col=draw_id_col
+        )
+        == expected
+    )
